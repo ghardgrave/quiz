352dcf48b3ba969d5c829680c6769ce59f792c59
ghardgrave <ghardgrave@gmail.com>
2016-04-27 13:48:04 -0700

    Wednesday AM tweaks

--- js/script.js
+++ js/script.js
@@ -1,22 +1,51 @@
 $(document).ready(function(){
 /* quiz functionality */
 
+
 $('.answer').click(function(){
 	$('.answer').removeClass('selected');
 	$(this).addClass( "selected" );
 	$('.rightarrow').fadeIn( 500 ).show();
+	console.dir(this);
+	$('.answer').removeClass('selectedAnswer');
+	$(this).addClass('selectedAnswer');
 });
+/* not sure about this part... */
+function answerChecker() {
+var questionNumber = $('.selectedAnswer')[0].parentElement.id;
+var questionAnswer = $('.selectedAnswer')[0].getAttribute("name");
+	
+	checkAnswer(questionNumber, questionAnswer);
 
+};
 
+function checkAnswer(question, answer){
+
+	if (question == "n1" && answer == "Chile"){
+		console.log("Answer 1 - right");
+
+	}
+	else if (question == "n1" && answer !== "Chile") {
+		console.log("Answer 1 - wrong");
+		
+	}
+	
+
+};
+/* not sure */
 
 
 
 /* navigation buttons and arrows */
 $('.myButton').click(function(){
+		$('.results').hide();
+		$('.numberCircleInverted').hide();
+		$('.numberCircle').show();
         $('.start').fadeOut ( 'slow' ).hide();
         $('.n1').fadeIn( 500 ).show();
+        $('.rightarrow').hide();
         $('#nc1').hide();
-        $('#nci1').fadeIn ( 500 ).show();
+        $('#nci1').show();
  		$('#nci1').css('display', 'inline-block');
 });
 
@@ -25,8 +54,9 @@ $('#a1').click(function(){
 	$('.n1').hide();
 	$('.n2').fadeIn( 500 ).show();
 	$('#nc2').hide();
-	$('#nci2').fadeIn ( 500 ).show();
+	$('#nci2').show();
  	$('#nci2').css('display', 'inline-block');
+ 	answerChecker();
 });
 
 $('#a2').click(function(){
@@ -34,18 +64,18 @@ $('#a2').click(function(){
 	$('.n2').hide();
 	$('.n3').fadeIn( 500 ).show();
 	$('#nc3').hide();
-	$('#nci3').fadeIn ( 500 ).show();
- 		$('#nci3').css('display', 'inline-block');
+	$('#nci3').show();
+ 	$('#nci3').css('display', 'inline-block');
 });
 
 
 $('#a3').click(function(){
-$('.rightarrow').hide();
+	$('.rightarrow').hide();
 	$('.n3').hide();
 	$('.n4').fadeIn( 500 ).show();
 	$('#nc4').hide();
-	$('#nci4').fadeIn ( 500 ).show();
- 		$('#nci4').css('display', 'inline-block');
+	$('#nci4').show();
+ 	$('#nci4').css('display', 'inline-block');
 
 
 });
@@ -55,13 +85,14 @@ $('.rightarrow').hide();
 	$('.n4').hide();
 	$('.n5').fadeIn( 500 ).show();
 	$('#nc5').hide();
-	$('#nci5').fadeIn ( 500 ).show();
- 		$('#nci5').css('display', 'inline-block');
+	$('#nci5').show();
+ 	$('#nci5').css('display', 'inline-block');
 });
 
 
 $('#a5').click(function(){
 	$('.n5').hide();
+	$('.results').fadeIn( 750 ).show();
 });
 /* end navigation */
 
