fbd8e96624d01bfb0694d4de5950fede6a691028
ghardgrave <ghardgrave@gmail.com>
2016-04-30 12:58:45 -0400

    Everything but final button and double-click

--- js/script.js
+++ js/script.js
@@ -1,99 +1,142 @@
 $(document).ready(function(){
-/* quiz functionality */
+/* questions object */
+var questions = [
+      {
+      	question: "What country is Easter Island part of?",
+        choices: ["Chile","Tahiti","Ecuador","Peru"],
+        qNum: 0,
+		answer: "Chile"      
+	},
+     	{
+      	question: "What are the giant statues on Easter Island called?",
+        choices: ["Maha","Moai","Toki","Pukao"],
+        qNum: 1,
+        answer: "Moai",
+      },
+      {
+      	question: "Which way do most of the statues face?",
+        choices: ["West","Toward each other","Toward the sea","Inland"],
+        qNum: 2,
+        answer: "Inland",
+      },
+      {
+        question: "What is the other name for Easter Island?",
+        choices: ["Ahu","Orango","Rapa Nui","Rapa Iti"],
+        qNum: 3,
+        answer: "Rapa Nui"
+      },
+      {
+       question: "What is the name of the only town on Easter Island?",
+       choices: ["Papeete","Tongariki","Anakena","Hanga Roa"],
+       qNum: 4,
+       answer: "Hanga Roa"
+      },
 
+];
 
-$('.answer').click(function(){
-	$('.answer').removeClass('selected');
-	$(this).addClass( "selected" );
-	$('.rightarrow').fadeIn( 500 ).show();
-	console.dir(this);
-	$('.answer').removeClass('selectedAnswer');
-	$(this).addClass('selectedAnswer');
-});
-/* not sure about this part... */
-function answerChecker() {
-var questionNumber = $('.selectedAnswer')[0].parentElement.id;
-var questionAnswer = $('.selectedAnswer')[0].getAttribute("name");
-	
-	checkAnswer(questionNumber, questionAnswer);
+/* global variables */
+var numberCorrect = 0;
+var currentQuestion = 0;
 
-};
+/* start-reset button */
 
-function checkAnswer(question, answer){
-
-	if (question == "n1" && answer == "Chile"){
-		console.log("Answer 1 - right");
-
-	}
-	else if (question == "n1" && answer !== "Chile") {
-		console.log("Answer 1 - wrong");
-		
-	}
-	
-
-};
-/* not sure */
-
-
-
-/* navigation buttons and arrows */
 $('.myButton').click(function(){
+/*		currentQuestion = 0;*/
+		nextQuestion();
 		$('.results').hide();
 		$('.numberCircleInverted').hide();
+		$("#nci1").show();
 		$('.numberCircle').show();
+		$('#nc1').hide();
+
         $('.start').fadeOut ( 'slow' ).hide();
-        $('.n1').fadeIn( 500 ).show();
-        $('.rightarrow').hide();
-        $('#nc1').hide();
+        $('#question-wrapper').fadeIn( 500 ).show();
+        
+   /*     $('#nc1').hide();
         $('#nci1').show();
- 		$('#nci1').css('display', 'inline-block');
-});
-
-$('#a1').click(function(){
-	$('.rightarrow').hide();
-	$('.n1').hide();
-	$('.n2').fadeIn( 500 ).show();
-	$('#nc2').hide();
-	$('#nci2').show();
- 	$('#nci2').css('display', 'inline-block');
- 	answerChecker();
-});
-
-$('#a2').click(function(){
-	$('.rightarrow').hide();
-	$('.n2').hide();
-	$('.n3').fadeIn( 500 ).show();
-	$('#nc3').hide();
-	$('#nci3').show();
- 	$('#nci3').css('display', 'inline-block');
+ 		$('#nci1').css('display', 'inline-block');*/
 });
 
 
-$('#a3').click(function(){
-	$('.rightarrow').hide();
-	$('.n3').hide();
-	$('.n4').fadeIn( 500 ).show();
-	$('#nc4').hide();
-	$('#nci4').show();
- 	$('#nci4').css('display', 'inline-block');
 
 
-});
+/* advances circled numbers, displays new question, */
 
-$('#a4').click(function(){
-$('.rightarrow').hide();
-	$('.n4').hide();
-	$('.n5').fadeIn( 500 ).show();
-	$('#nc5').hide();
-	$('#nci5').show();
- 	$('#nci5').css('display', 'inline-block');
-});
+function nextQuestion(){
 
 
-$('#a5').click(function(){
-	$('.n5').hide();
-	$('.results').fadeIn( 750 ).show();
-});
-/* end navigation */
+		console.log('current question is '+currentQuestion);  
+
+		$('.rightarrow').hide();
+
+		numberAdvance();
+		/* display new question */
+	/*	$("#question-wrapper").html(newQuestion);*/
+		var newQuestion = '<div class="q">'+questions[currentQuestion].question+'<br><br></div><div class="answer answer0">'+questions[currentQuestion].choices[0]+'</div><div class="answer answer1">'+questions[currentQuestion].choices[1]+'</div><div class="answer answer2">'+questions[currentQuestion].choices[2]+'</div><div class="answer answer3">'+questions[currentQuestion].choices[3]+'</div><div class="rightarrow"><img src="images/rtarrow.png" width=50px class="RA"></div>';
+
+		$("#question-wrapper").html(newQuestion);
+
+		$('.answer').click(function(){
+			$('.answer').removeClass('selectedAnswer');
+			$(this).addClass('selectedAnswer');
+			$('.rightarrow').fadeIn( 500 ).show();
+	/*		console.dir(this); */
+		});
+
+	$('.RA').click(function(){
+		righton();
+	});
+
+	
+
+}
+
+
+function righton(){
+
+	if (currentQuestion < 4) {
+	score(); 
+	currentQuestion++;  
+	nextQuestion();
+}
+
+	else {
+
+		$(".RA").click(function(){
+			score();
+				
+    			$('.results').show; 
+       			var finalScore = '<div class="results"><h2>RESULTS</h2> You got '+numberCorrect+' question(s) right! <a href="#" class="myButton">Try Again</a></div>'
+
+    			$("#question-wrapper").html(finalScore);
+			
+			});
+
+	}
+}
+
+function numberAdvance() {
+		/* advancing circled numbers */
+		var numcirc = questions[currentQuestion].qNum + 1;
+		$("#nc" + numcirc).hide();
+		$("#nci" + numcirc).show();
+		$("#nci" + numcirc).css('display', 'inline-block');
+
+}
+
+function score(){ 
+ 
+ /* answer of currentquestion*/
+		var text = $('.selectedAnswer').text();
+
+		if(text === questions[currentQuestion].answer){
+       			alert("Yes! "+text+"is correct!");
+       			numberCorrect++;
+       			console.log("number correct is "+numberCorrect);
+       			
+        }       else{
+        		alert("no, "+text+" is wrong");
+        }
+}
 
 });
